ddf33d740b17b3abc80b959711770437689ee1cc
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 21:25:39 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const app = express();
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
+
+const staticFileMiddleware = express.static(__dirname + '/www/dist/');
+app.use(staticFileMiddleware);
+app.use(express.json());
+app.use(express.urlencoded());
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+    key: fs.readFileSync('./pvt/privkey.pem'),
+    cert: fs.readFileSync('./pvt/fullchain.pem'),
+}, app);
+
+httpServer.listen(HTTP_PORT, () => {
+   console.log('[Http] Server running on port: ' + HTTP_PORT);
+});
+httpsServer.listen(HTTPS_PORT, () => {
+    console.log('[Https] Server running on port: ' + HTTPS_PORT);
+});
