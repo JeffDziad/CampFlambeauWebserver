52293344bd63def789eb2f9e12e5c7699494eac9
BuildTools <unconfigured@null.spigotmc.org>
2022-04-10 21:08:07 -0500

    - Updates

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const router = express.Router();
+const nodemailer = require('nodemailer');
 const history = require('connect-history-api-fallback');
 const app = express();
 const HTTP_PORT = 80;
@@ -11,6 +13,16 @@ const staticFileMiddleware = express.static(__dirname + '/www/dist/');
 app.use(staticFileMiddleware);
 app.use(express.json());
 app.use(express.urlencoded());
+
+router.get('/hello', (req, res) => {
+    res.send('Howdy!');
+});
+
+router.post('/submit/inquiry', (req, res) => {
+    let success = false;
+    console.log(req.body);
+    res.send({success: success})
+});
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
