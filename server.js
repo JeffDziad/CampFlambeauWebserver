664fd6ff25334d9f863b371827376cfd82a7d779
BuildTools <unconfigured@null.spigotmc.org>
2022-04-10 21:55:23 -0500

    - Updates

--- server.js
+++ server.js
@@ -14,14 +14,33 @@ app.use(staticFileMiddleware);
 app.use(express.json());
 app.use(express.urlencoded());
 
+const emailSender = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: '',
+        pass: '',
+    }
+});
+
 app.get('/hello', (req, res) => {
     res.send('Howdy!');
 });
 
-app.post('/submit/inquiry', (req, res) => {
-    let success = false;
-    console.log(req.body);
-    res.send({success: success})
+app.post('/submit/inquiry', async (req, res) => {
+    let form_data = req.body;
+    try {
+        await emailSender.sendMail({
+            from: 'campflambeau@gmail.com',
+            to: 'campflambeau@gmail.com',
+            subject: 'CAMP FLAMBEAU INQUIRY SUBMISSION',
+            text: `Start Date: ${form_data.startDate}, End Date: ${form_data.endDate} | Start Message | ${form_data.message} | Submitted by: ${form_data.name} ($${form_data.email})`,
+        });
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+    res.sendStatus(200);
 });
 
 const httpServer = http.createServer(app);
